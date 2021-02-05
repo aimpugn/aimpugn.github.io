29bde9590ec94ccf12e17bbdacadd1a113a3590b
aimpugn <aimpugn@gmail.com>
2021-02-06 00:02:27 +0900

    괄호 수학 수식 구별자 display true 변경

--- docs/assets/js/mathjax.js
+++ docs/assets/js/mathjax.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', (e) => {
     renderMathInElement(document.body, {
         delimiters: [
             {left: "$$", right: "$$", display: true},
-            {left: "\\(", right: "\\)", display: false},
+            {left: "\\(", right: "\\)", display: true},
             {left: "\\[", right: "\\]", display: true}
         ],
         ignoredTags: ["script", "noscript", "style", "option"],
         leqno: true,    // If true, display math has \tags rendered on the left instead of the right
         fleqn: true     // If true, display math renders flush left with a 2em left margin
     });
-    renderKatexMathjaxFallback(document.body);
 });
