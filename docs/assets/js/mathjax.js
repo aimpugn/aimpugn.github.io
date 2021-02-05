68a2caed2e35600b5460dc3243bf5a08b6ac161a
aimpugn <aimpugn@gmail.com>
2021-02-05 23:51:29 +0900

    Katex 렌더 이슈

--- docs/assets/js/mathjax.js
+++ docs/assets/js/mathjax.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', (e) => {
             {left: "\\(", right: "\\)", display: false},
             {left: "\\[", right: "\\]", display: true}
         ],
-        ignoredTags: ["script", "noscript", "style", "textarea", "pre", "code", "option"],
+        ignoredTags: ["script", "noscript", "style", "option"],
         leqno: true,    // If true, display math has \tags rendered on the left instead of the right
         fleqn: true     // If true, display math renders flush left with a 2em left margin
     });
+    renderKatexMathjaxFallback(document.body);
 });
