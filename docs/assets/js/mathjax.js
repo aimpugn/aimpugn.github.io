b143f150f9e185b683047253f636398f6a950330
aimpugn <aimpugn@gmail.com>
2021-02-06 00:40:05 +0900

    인라인 식별자 $$ 추가

--- docs/assets/js/mathjax.js
+++ docs/assets/js/mathjax.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     renderMathInElement(document.body, {
         delimiters: [
             {left: "$$", right: "$$", display: true},
+            {left: "$", right: "$", display: false},
             {left: "\\(", right: "\\)", display: true},
             {left: "\\[", right: "\\]", display: true}
         ],
