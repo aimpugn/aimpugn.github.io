dae091753bb91993cc6370d6bb7e38e77cbb7172
aimpugn <aimpugn@gmail.com>
2021-02-17 23:34:47 +0900

    github page
    mathjax.js에 delimiter 추가

--- docs/assets/js/mathjax.js
+++ docs/assets/js/mathjax.js
@@ -3,6 +3,11 @@ function renderMathInElementConfig(){
     renderMathInElement(document.body, {
         delimiters: [
             {left: "$$", right: "$$", display: true},
+            /**
+             * 없으면 github page에서 아래와 같이 나온다
+             * \[\text{load factor} = \frac{\text{저장된 데이터 개수}}{\text{버킷의 개수}} = \frac{n}{k}\]
+             */
+            {left: "\\[", right: "\\]", display: true},
             // https://stackoverflow.com/a/45301641 
             // inline에서 사용 시 display는 false
             {left: "$", right: "$", display: false}
